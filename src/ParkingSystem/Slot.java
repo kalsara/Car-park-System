458dc46077c11c8322a828b02fefdf530fb7e38a
kalsara <kalsaramagamage@gmail.com>
2017-02-13 13:41:10 +0530

    getter setters slot class and implement it

--- src/ParkingSystem/Slot.java
+++ src/ParkingSystem/Slot.java
@@ -1,8 +1,24 @@
 package ParkingSystem;
 
-import java.io.Serializable;
 
-public class Slot implements Serializable{
+public class Slot {
 
-    
+    private int slotId;
+    private Vehicle vehicle;
+
+    public int getSlotId() {
+        return slotId;
+    }
+
+    public void setSlotId(int slotId) {
+        this.slotId = slotId;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
 }
